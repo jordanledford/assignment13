2bf4c99b2c324a6db4b64510207c1ef688def2ed
J <jordanmledford@gmail.com>
2016-11-18 21:10:29 -0500

    fixed problems

--- main.js
+++ main.js
@@ -1,99 +1,83 @@
 
-document.querySelector("#inject-html button").addEventListener('click',function(){
+document.querySelector("#inject-html button").addEventListener('click', function(){
   // TASK #1
-  var answerBoxEl = document.querySelector('#inject-html .answer-box')
+  var answerBoxEl = document.querySelector('#inject-html .answer-box');
   answerBoxEl.innerHTML = "<h2>Here is some text for you!!</h2>"
 })
 
-document.querySelector("#double button").addEventListener('click',function doubler(num){
-  var doubleNumber = document.getElementById("compoundInvestment")
-  var doubleMan = doublenumber.innerHMTL.parseInt()
-  doubleMan(int){
-    num * 2 = var newNum;
-    return newNum;
-  }
+document.querySelector("#double button").addEventListener('click', function(num){
+  var pTagVal = document.querySelector('#compoundInvestment')
+  // you do the rest
+  var currentInt = pTagVal.innerHTML
+  var currentInt = parseInt(currentInt);
 
- // while (num != Infinity){
-//    var a = 1, b = 2, num = 1;
-//    for (var i = 2; i <= num; i++){
-//      num = a * b;
-//      a = b;
-//      b = num;
-//    }
-//    {return num;
-//    }
-//  }
-// });
-
-document.querySelector("#color-circle button").addEventListener('click',function flipper(onClick){
-  if ( document.getElementById("#circle-bw").innerHTML = " ") {
-    document.getElementById("circle-bw").innerHTML = "#000";
-  };
-  if (document.getElementById("#circle-bw").innerHTML = " ") {
-    document.getElementById("#circle-bw").innerHTML = "#000";
-  }
+  pTagVal.innerHTML = currentInt * 2;
 })
+
+document.querySelector("#color-circle button").addEventListener('click', function(){
+  var circle = document.querySelector("#circle-bw");
+    if (circle.style.background === "rgb(255, 255, 255)") {
+      circle.style.background = "rgb(0, 0, 0)";
+    } else {
+      circle.style.background = "rgb(255, 255, 255)";
+    }
+})
+
 
 document.querySelector("#blow-up button").addEventListener('click',function(onClick){
-  // TASK #4}
-  var redCircle =  document.querySelector(".circle-red");
+  var redCircle = document.querySelector('.circle-red');
+  var elStyles = window.getComputedStyle(redCircle);
+  var heightNumberString = elStyles.height.slice(0, -2);
+  var widthNumberString = elStyles.width.slice(0, -2);
+  var heightNumber = parseInt(elStyles.height);
+  var widthNumber = parseInt(elStyles.width);
 
-  var elementStyles =  window.getComputedStyle(redCircle);
-  	console.log('w', elementStyles.width);
-    console.log('h', elementStyles.height);
-    var elWidthString = elementStyles.width;
-  	var elHeightString =  elementStyles.height;
-  var charArrayW = elWidthString.slice(0, -2);
-  var charArrayH = elHeightString.slice(0, -2);
-  charArrayW.pop();
-  charArrayH.pop();
-  var currentCircleW = parseInt(charArrayW);
-  var currentCircleH = parseInt(charArrayH);
-  var doubleWidth = currentCircleW * 2;
-  var doubleHeight = currentCircleH * 2;
-
-  redCircle.style.width = doubleWidth + "px";
-  redCircle.style.height = doubleHeight + "px";
-  if (doubleWidth >= 320){
-  redCircle.style.width = 40 + "px";
-  redCircle.style.height = 40 + "px";
+  if (heightNumber <= 320) {
+    redCircle.style.height = heightNumber * 2 + 1 + 'px';
+    redCircle.style.width = widthNumber * 2 + 1 + 'px';
   } else {
-  return;
+    redCircle.style.height = "40" + 'px';
+    redCircle.style.width = "40" + 'px';
   }
-
 })
 
-document.querySelector("#student workspace #remove-button").addEventListener('click',function(){
+document.querySelector("#remove button").addEventListener('click',function(){
   // TASK #5
   var inactiveParent = document.getElementById("user-list");
   var inactiveChild = document.getElementsByClassName("inactive");
-   inactiveParent.removeChild(inactiveChild);
-})
-
+  var userLists = document.querySelector('#user-list')
+  var listOfUsers = document.querySelectorAll('#user-list li')
+  for (var i = 0; i < listOfUsers.length; i += 1 ){
+    var listers = listOfUsers[i];
+    if(listers.classList.contains('active') === false){
+      userLists.removeChild(listers);
+    }
+  }
+});
 
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
-  // TASK #6
-  // DRY DRY DRY DRY
-  var arrayOfBoxes = [one, two, three, four, five, six, seven]
-  var someStuff = document.getElementsByClassName(boxes)
-    if (arrayOfBoxes[six] = true){}
-
-})
-
-document.querySelector("#pig-latin button").addEventListener('click',function(){
-  // TASK #7
-  var pigTasks = document.getElementById(tasks)
-
-  function pigIterator(stuff){
-    for (var tasks in pigTasks){
-      if (!pigTasks.hasOwnProperty(tasks)){
-        continue;
-      }
-      (i = 0 ;i < pigTasks;i++)
-    }
+  var boxContainer = document.querySelector('#reverse-squares .answer-box')
+  var boxLi = document.querySelectorAll('#reverse-squares span')
+  for (var i = boxLi.length -1; i >= 0; i--){
+     boxContainer.removeChild(boxLi[i])
+     boxContainer.appendChild(boxLi[i])
   }
 })
 
-document.querySelector("#cycle-image button").addEventListener('click',function(){
-   //TASK #8
+document.querySelector("#pig-latin button").addEventListener('click',function(){
+   var taskContainer = document.querySelectorAll('#tasks li')
+   console.log(taskContainer[1].innerHTML)
+   for (var i = 0; i < taskContainer.length; i++){
+      var currentArr = taskContainer[i].innerHTML
+      var newString = ''
+      for (var a = currentArr.length -1; a >= 0; a = a -1){
+         console.log(currentArr[a])
+         newString = newString + currentArr[a]
+      }
+      taskContainer[i].innerHTML = newStrg
+   }
 })
+
+// document.querySelector("#cycle-image button").addEventListener('click',function(){
+//    //TASK #8
+// })
